0f2dc85d1ac0b384bfaa8702d3ee7b1e989ac831
Steve Ruben <stevetuenkam@gmail.com>
2020-12-25 13:05:42 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Home from './components/home';
+
+
+//main app 
+class App extends Component {
+
+  render() {
+    return(
+      <div>
+      React
+      <Home/>
+      </div>
+    )}
+}
+
+
+export default App;
